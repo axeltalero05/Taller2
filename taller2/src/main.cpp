8b6a2ce1fc78444012242efa3c124d76522ff70e
axeltalero5 <axel.talero@cun.edu.co>
2019-09-02 17:04:14 -0500

    Cambio de estado por Flanco

--- taller2/src/main.cpp
+++ taller2/src/main.cpp
@@ -3,6 +3,10 @@
 #define Button 5
 #define LED 4
 
+bool Last_Button_Value = 0;
+bool Button_Value;
+bool Led_State = 0;
+
 void setup() {
 Serial.begin(9600);
 pinMode(Button, INPUT);
@@ -14,6 +18,9 @@ digitalWrite(LED,LOW);
 void loop() {
  bool Button_Value = digitalRead(Button);
  Serial.println("Estado del pulsador: " + String(Button_Value));
- if (Button_Value == 1) digitalWrite(LED,HIGH);
-    else digitalWrite (LED,LOW);
+ if (Last_Button_Value == 0 && Button_Value == 1) {
+    Led_State = !Led_State;
+    digitalWrite(LED, Led_State);
+ }
+ Last_Button_Value = Button_Value ;
 }
