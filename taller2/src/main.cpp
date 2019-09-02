d1a8760c419fa9d5a54bd94e4a836f9068e13ed6
axeltalero5 <axel.talero@cun.edu.co>
2019-09-02 17:11:39 -0500

    Salida Analoga
    Valor del potenciometro

--- taller2/src/main.cpp
+++ taller2/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 
 #define Analog_Pin A0
-
+#define LED 4
 
 void setup() {
 Serial.begin(9600);
-
+pinMode(LED, OUTPUT);
 }
 
 void loop() {
  int Value_Pot = analogRead( Analog_Pin);
+ analogWrite(LED, Value_Pot / 4);
  Serial.println("Valor del potenciometro: " + String(Value_Pot));
 }
