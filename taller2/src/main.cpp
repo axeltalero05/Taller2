7d40290ec5d5f363fb1e0780b1f68daaeeb07f87
axeltalero5 <axel.talero@cun.edu.co>
2019-09-02 17:09:31 -0500

    Entrada Analoga
    Valor de potenciometro

--- taller2/src/main.cpp
+++ taller2/src/main.cpp
@@ -1,26 +1,14 @@
 #include <Arduino.h>
 
-#define Button 5
-#define LED 4
+#define Analog_Pin A0
 
-bool Last_Button_Value = 0;
-bool Button_Value;
-bool Led_State = 0;
 
 void setup() {
 Serial.begin(9600);
-pinMode(Button, INPUT);
-pinMode(LED, OUTPUT);
-digitalWrite(LED,LOW);
 
 }
 
 void loop() {
- bool Button_Value = digitalRead(Button);
- Serial.println("Estado del pulsador: " + String(Button_Value));
- if (Last_Button_Value == 0 && Button_Value == 1) {
-    Led_State = !Led_State;
-    digitalWrite(LED, Led_State);
- }
- Last_Button_Value = Button_Value ;
+ int Value_Pot = analogRead( Analog_Pin);
+ Serial.println("Valor del potenciometro: " + String(Value_Pot));
 }
